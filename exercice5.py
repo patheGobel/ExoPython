fe6023277ec3b30c26d214276c786aa13dd1b466
Pathé BA <pathegba@gmail.com>
2023-10-12 17:14:25 +0000

    voila une solution très originale

---
+++ exercice5.py
@@ -0,0 +1,20 @@
+#Exercise 5: Display all duplicate items from a list
+"""Exercise 4: Reverse Dictionary mapping
+Given:
+sample_list = [10, 20, 60, 30, 20, 40, 30, 60, 70, 80]
+Expected Output:
+[20, 60, 30]
+"""
+def displayAllDuplicateItemsFromList(liste):
+    doublons = set()
+    elements_vus = set()
+
+    for element in liste:
+        if element in elements_vus:
+            doublons.add(element)
+        else:
+            elements_vus.add(element)
+
+    return list(doublons) 
+sample_list = [10, 20, 60, 30, 20, 40, 30, 60, 70, 80]
+print(f"{displayAllDuplicateItemsFromList(sample_list)}")
